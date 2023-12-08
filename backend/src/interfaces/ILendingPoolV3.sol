5724ff8d6bb718fd40457e9e79ffa8a462ce9b86
viraj124 <virajm72@gmail.com>
2023-12-08 20:39:40 +0530

    chore: add interfaces

---
+++ backend/src/interfaces/ILendingPoolV3.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.20;
+
+interface ILendingPoolV3 {
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+
+    function withdraw(address asset, uint256 amount, address to) external;
+
+    function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external;
+}
