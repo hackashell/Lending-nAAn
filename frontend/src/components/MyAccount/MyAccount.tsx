d8792d1b1a8f7000ab93162515bbff2c420fb898
Petar Popovic <petarpnp@gmail.com>
2023-12-08 23:55:29 +0530

    Fix myaccount component height

--- frontend/src/components/MyAccount/MyAccount.tsx
+++ frontend/src/components/MyAccount/MyAccount.tsx
@@ -2,7 +2,7 @@ import Avatar from "boring-avatars";
 
 const MyAccount = ({address}) => (
     <div
-        className={`flex items-center gap-3 px-10 py-5 bg-[#222222] rounded-lg flex items-center justify-center cursor-pointer`}
+        className={`flex items-center gap-3 px-10 py-4 bg-[#222222] rounded-lg flex items-center justify-center cursor-pointer`}
     >
         <p className="text-sm text-white">{address}</p>
         {/* TODO: fetch ens image with graph or airstack and display that, fallback to boring avatar if no image is set */}
