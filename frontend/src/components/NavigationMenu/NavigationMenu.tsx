156b517ae621090cc87b393ee05f2d5f1603d345
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-12-09 19:34:58 +0530

    use demo example

--- frontend/src/components/NavigationMenu/NavigationMenu.tsx
+++ frontend/src/components/NavigationMenu/NavigationMenu.tsx
@@ -15,6 +15,7 @@ import {
 } from "../ui/dialog";
 // import { safeAuthInitOptions, safeAuthPack } from "../../../safeConfig";
 import { AuthKitSignInData, SafeAuthInitOptions, SafeAuthPack, SafeAuthUserInfo } from "@safe-global/auth-kit";
+import { useAccountAbstraction } from "../../../store/accountAbstractionContext";
 
 const NavigationMenu = () => {
   const [account, setAccount] = useState<string>();
@@ -23,12 +24,13 @@ const NavigationMenu = () => {
   const [safeAuthPack, setSafeAuthPack] = useState<SafeAuthPack>()
   const [safeAuthSignInResponse, setSafeAuthSignInResponse] = useState<AuthKitSignInData>(
   )
-  const [isAuthenticated, setIsAuthenticated] = useState(!!safeAuthPack?.isAuthenticated)
+  // const [isAuthenticated, setIsAuthenticated] = useState(!!safeAuthPack?.isAuthenticated)
   const [safeProvider, setSafeProvider] = useState<BrowserProvider>()
 
   const [userInfo, setUserInfo] = useState<SafeAuthUserInfo | null>(null)
-  const [safeChainId, setSafeChainId] = useState<string>()
+  // const [safeChainId, setSafeChainId] = useState<string>()
   const [balance, setBalance] = useState<string>()
+  const { loginWeb3Auth, isAuthenticated, chainId: safeChainId } = useAccountAbstraction()
 
   const Auth = Buffer.from(
     process.env.NEXT_PUBLIC_INFURA_API_KEY +
@@ -65,95 +67,7 @@ const NavigationMenu = () => {
     }
   };
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const safeAuthInitOptions: SafeAuthInitOptions = {
-  //       enableLogging: true,
-  //       showWidgetButton: false,
-  //       chainConfig: {
-  //         chainId: '0xaa36a7',
-  //         rpcTarget: `https://rpc.sepolia.org	`
-  //       },
-  //     }
-  //      const safeAuthPack = new SafeAuthPack()
-  //     await safeAuthPack.init(safeAuthInitOptions);
-  //     setSafeAuthKit(safeAuthPack)
-  //   })();
-  // }, []);
-
-  useEffect(() => {
-    // @ts-expect-error - Missing globals
-    const params = new URL(window.document.location).searchParams
-    const chainId = params.get('chainId')
-
-    ;(async () => {
-      const options: SafeAuthInitOptions = {
-        enableLogging: true,
-        buildEnv: 'production',
-        chainConfig: {
-          chainId: chainId || '0x64',
-          rpcTarget: 'https://gnosis.drpc.org'
-        }
-      }
-
-      const authPack = new SafeAuthPack()
-
-      await authPack.init(options)
-
-      console.log('safeAuthPack:safeEmbed', authPack.safeAuthEmbed)
-
-      setSafeAuthPack(authPack)
-
-      authPack.subscribe('accountsChanged', async (accounts) => {
-        console.log('safeAuthPack:accountsChanged', accounts, authPack.isAuthenticated)
-        if (authPack.isAuthenticated) {
-          const signInInfo = await authPack?.signIn()
-
-          setSafeAuthSignInResponse(signInInfo)
-          setIsAuthenticated(true)
-        }
-      })
-
-      authPack.subscribe('chainChanged', (eventData) =>
-        console.log('safeAuthPack:chainChanged', eventData)
-      )
-    })()
-  }, [])
-
-  useEffect(() => {
-    if (!safeAuthPack || !isAuthenticated) return
-    ;(async () => {
-      const web3Provider = safeAuthPack.getProvider()
-      const userInfo = await safeAuthPack.getUserInfo()
-
-      setUserInfo(userInfo)
-
-      if (web3Provider) {
-        const provider = new BrowserProvider(safeAuthPack.getProvider() as Eip1193Provider)
-        const signer = await provider.getSigner()
-        const signerAddress = await signer.getAddress()
-
-        setSafeChainId((await provider?.getNetwork()).chainId.toString())
-        setBalance(
-          ethers.formatEther((await provider.getBalance(signerAddress)) as ethers.BigNumberish)
-        )
-        setSafeProvider(provider)
-      }
-    })()
-  }, [isAuthenticated])
-
-  const loginWIthSafe = async () => {
-    const signInInfo = await safeAuthPack?.signIn()
-
-    setSafeAuthSignInResponse(signInInfo)
-    setIsAuthenticated(true)
-  }
-
-  const logout = async () => {
-    await safeAuthPack?.signOut()
-
-    setSafeAuthSignInResponse(undefined)
-  }
+  
 
   const getUserInfo = async () => {
     const userInfo = await safeAuthPack?.getUserInfo()
@@ -214,7 +128,7 @@ const NavigationMenu = () => {
                   />
                   <AnimatedButton
                     text="Login with Safe"
-                    onClick={loginWIthSafe}
+                    onClick={loginWeb3Auth}
                     className="w-2/3 m-auto h-12 text-base"
                   />
                 </div>
---
+++ frontend/src/utils/getChain.ts
@@ -0,0 +1,10 @@
+import chains from "@/constants/chains";
+
+
+const getChain = (chainId?: string) => {
+  const chain = chains.find((chain) => chain.id === chainId)
+
+  return chain
+}
+
+export default getChain
