b7e0715f88e0ec3f06d3809856f13fd7792cba74
Petar Popovic <petarpnp@gmail.com>
2023-12-09 17:17:50 +0530

    Hide user borrows and supplies if not connected

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,24 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navigation from "@/components/Navigation/Navigation";
 import { BorrowSupply } from '@/components/BorrowSupply/BorrowSupply';
 import UserBorrowsAndSupplies from "@/components/UserBorrowsAndSupplies/UserBorrowsAndSupplies";
+import {useSDK} from "@metamask/sdk-react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { account, connected } = useSDK();
+  // TODO: This checks if user is singed in with MetaMask, we will need to add OR check here for Safe AA
+  const loggedIn = account && connected;
+
   return (
     <main>
       <Navigation />
       <BorrowSupply />
-      <UserBorrowsAndSupplies />
+      {
+        loggedIn &&
+        <UserBorrowsAndSupplies />
+      }
     </main>
   )
 }
