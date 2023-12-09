71d4bb2707f8df40b7a3c902ed5d283d2a6c2882
Petar Popovic <petarpnp@gmail.com>
2023-12-09 19:03:29 +0530

    Chnage gas loader style

--- frontend/src/components/Spinner/index.tsx
+++ frontend/src/components/Spinner/index.tsx
@@ -1,9 +1,9 @@
-export const Spinner = () => {
+export const Spinner = ({className}) => {
   return (
-    <div role="status">
+    <div className={className} role="status">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="text-gray-200 animate-spin dark:text-gray-600 fill-pink-500"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
