71328ea09cb60b57cf952f0cd4d170a212b0210b
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-12-09 16:11:26 +0530

    Update Button.tsx

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,10 +1,26 @@
-const Button = ({text, onClick, primary, className}) => (
-    <button
-        onClick={onClick}
-        className={`${className} text-sm px-10 py-5 text-white hover:opacity-90 ${primary ? "bg-[--primary-color]" : "bg-[#222222]"} border ${primary ? "border-[--primary-color]" : "border-[#222222]"} rounded-lg flex items-center justify-center hover:cursor-pointer`}
-    >
-        {text}
-    </button>
+interface Props {
+  text: string;
+  onClick: () => void;
+  className?: string;
+  primary?: boolean;
+}
+
+const Button: React.FC<Props> = ({
+  text,
+  onClick,
+  primary = false,
+  className,
+}) => (
+  <button
+    onClick={onClick}
+    className={`${className} text-sm px-10 py-5 text-white hover:opacity-90 ${
+      primary ? "bg-[--primary-color]" : "bg-[#222222]"
+    } border ${
+      primary ? "border-[--primary-color]" : "border-[#222222]"
+    } rounded-lg flex items-center justify-center hover:cursor-pointer`}
+  >
+    {text}
+  </button>
 );
 
 export default Button;
