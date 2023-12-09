7bff3fd32733cdc590a020081d64a0f2557529e8
qedk <1994constant@gmail.com>
2023-12-09 13:50:23 +0530

    feat: fmt

--- backend/src/Oven.sol
+++ backend/src/Oven.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import {Multicall} from "lib/openzeppelin-contracts/contracts/utils/Multicall.sol";
-import {IAaveAdapter} from "./interfaces/IAaveAdapter.sol";
-import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { Multicall } from "lib/openzeppelin-contracts/contracts/utils/Multicall.sol";
+import { IAaveAdapter } from "./interfaces/IAaveAdapter.sol";
+import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract Oven is Multicall, Ownable2Step {
     enum Flavor {
@@ -26,15 +26,9 @@ contract Oven is Multicall, Ownable2Step {
         IAaveAdapter(adapters[Flavor.Aave]).redeem(IERC20(token), amount, msg.sender);
     }
 
-    function aaveBorrow(address token, uint256 amount) external {
+    function aaveBorrow(address token, uint256 amount) external { }
 
-    }
+    function aaveRepay(address token, uint256 amount) external { }
 
-    function aaveRepay(address token, uint256 amount) external {
-
-    }
-
-    function oneInchSwap(address tokenIn, address tokenOut, uint256 amountIn, uint256 minAmountOut) external {
-
-    }
+    function oneInchSwap(address tokenIn, address tokenOut, uint256 amountIn, uint256 minAmountOut) external { }
 }
